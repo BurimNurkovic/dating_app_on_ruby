090166c2073f7a9ba67db8b1238ff6208ad6f5fb
Marc Rochon <marc.88@mail.com>
2016-10-27 20:17:46 +0200

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+  before_action :require_login
+  before_action :set_user, only:[:edit, :profile, :update, :destroy, :get_email]
+
+  def index
+    
+    if params[:id]
+      @users = User.gender(current_user).where('id < ?', params[:id]).not_me(current_user).limit(10) - current_user.matches(current_user)
+    else
+      @users = User.gender(current_user).not_me(current_user).limit(10) - current_user.matches(current_user)
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+  end
+      
+  end
+
+  def edit
+     authorize! :update, @user
+  end
+
+  def profile
+  end
+
+  def update
+    if @user.update(user_params)
+      respond_to do |format|
+        format.html {redirect_to users_path}
+      end
+    else
+      redirect_to edit_user_path(@user)
+    end
+  end
+  
+  def destroy
+    if @user.destroy
+      redirect_to root_path
+      session[:user_id]  = nil
+      session[:omniauth] = nil
+    else
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+  def matches(current_user)
+    friendships.where(state: "pending").map(&:friend) + current_user.friendships.where(state: "ACTIVE").map(&:friend) + current_user.inverse_friendships.where(state: "ACTIVE").map(&:user) 
+  end
+
+
+  def get_email
+      respond_to do |format|
+        format.js
+      end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:interest, :bio, :image, :location, :date_of_birth)
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,81 @@
+class User < ActiveRecord::Base
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  
+  has_attached_file :avatar, 
+                  :storage  => :s3, 
+                  :styles => { :medium => "370x370", :thumb => "100x100" },
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  default_scope { order('id DESC') }
+
+	def self.sign_in_from_omniauth(auth)
+		find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+	end
+
+	def self.create_user_from_omniauth(auth)
+        create(
+          avatar: process_uri(auth['info']['image'] + "?width=9999"),
+          email: auth['info']['email'],
+          provider: auth['provider'],
+          uid: auth['uid'],
+          name: auth['info']['name'],
+          gender: auth['extra']['raw_info']['gender'],
+          date_of_birth: auth['extra']['raw_info']['birthday'].present? ? Date.strptime( auth['extra']['raw_info']['birthday'], '%m/%d/%Y') : nil,
+          location: auth['info']['location'],
+          bio: auth['extra']['raw_info']['bio']
+        )
+	end
+
+  # Friendship Methods
+
+  def request_match(user2)
+    self.friendships.create(friend: user2)
+  end
+
+  def accept_match(user2)
+    self.friendships.where(friend: user2).first.update_attribute(:state, "ACTIVE")
+  end
+
+  def remove_match(user2)
+    inverse_friendship = inverse_friendships.where(user_id: user2).first
+
+    if inverse_friendship
+      self.inverse_friendships.where(user_id: user2).first.destroy
+    else
+      self.friendships.where(friend_id: user2).first.destroy
+    end
+    
+  end
+
+   #/ Query Methods \#
+  
+  def self.gender(user)
+    
+    case user.interest
+      when "Male"
+        where('gender = ?', 'male')
+      when "Female"
+        where('gender = ?', 'female')
+      else
+        all
+    end
+
+  end
+
+  def self.not_me(current_user)
+     where.not(id: current_user.id) 
+  end
+
+
+  private
+
+  def self.process_uri(uri)
+      image_url = URI.parse(uri)
+      image_url.scheme = 'https'
+      image_url.to_s
+  end
+
+
+
+end
